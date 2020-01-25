877bcab93c89b43bd7bdf39251ee3dffa6fe7c76
Emily Hernandez <emily.hernandez@ukkopro.fi>
2020-01-25 19:59:53 +0200

    Add vue composition API and set TailwindCSS

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="w-full text-red-600">Navigation</div>
+    <HelloWorld/>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
+import VueCompositionApi from '@vue/composition-api'
 import App from './App.vue'
+import "./styles/main.scss"
 
+Vue.use(VueCompositionApi)
 Vue.config.productionTip = false
 
 new Vue({
