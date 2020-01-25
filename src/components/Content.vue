9e526fe5c3a5b24bd3608c5096bda03990f93fe4
Emily Hernandez <emily.hernandez@ukkopro.fi>
2020-01-25 21:21:04 +0200

    Update content section

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,24 +1,48 @@
 <template>
-  <div class="hello">
+  <div class="w-full flex flex-col items-center">
+    <div class="max-w-4xl">
+      <NewsArticle
+        v-for="(article, index) in state.articles"
+        :key="`article-${index + 1}`"
+        :title="article.title"
+        :description="article.description"
+        class="my-4 border-b border-gray-200 py-4"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import { onMounted } from '@vue/composition-api'
+import { reactive, onMounted } from '@vue/composition-api'
+import response from '../testdata.json'
+import NewsArticle from './NewsArticle'
 
 export default {
+  components: {
+    NewsArticle
+  },
   setup() {
+    let state = reactive({
+      articles: []
+    })
+
     onMounted(() => {
-      /* eslint no-console: "off" */
-      // console.log('News api key: ', process.env.VUE_APP_NEWS_API_KEY)
       // let headers = new Headers()
       // headers.append('Authorization', process.env.VUE_APP_NEWS_API_KEY)
 
       // let request = new Request('https://newsapi.org/v2/everything?q=nepal', {
-      //   headers
+        //   headers
       // })
       // fetch(request)
+      /* eslint no-console: "off" */
+      console.log('Content: ', response.articles)
+
+      state.articles = response.articles
     })
+
+    return {
+      state
+    }
   }
 }
 </script>
