c3c701b02ed9b9d1ba3de33897a6b0b670a3bab9
Emily Hernandez <emily.hernandez@ukkopro.fi>
2020-01-25 21:20:52 +0200

    Create reusable article component

---
+++ src/components/NewsArticle.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="w-full">
+    <h3 class="text-3xl font-gray-800 mb-2">{{ title }}</h3>
+    <p>{{ description }}</p>
+    <AppButton>Read full article</AppButton>
+  </div>
+</template>
+
+<script>
+// import { reactive, onMounted } from '@vue/composition-api'
+import AppButton from './AppButton'
+
+export default {
+  components: {
+    AppButton
+  },
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    }
+  },
+  setup() {
+    return {}
+  }
+}
+</script>
